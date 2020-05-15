3ce9ef9f2fd584cfd5c71b8b827a0a847f8ae982
eyupkayadarcin <eyup@jointventures.io>
2020-05-15 10:36:59 +0300

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,30 @@
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+
+import Header from './header';
+
+// Code-splitting is automated for routes
+import Home from '../routes/home';
+import Profile from '../routes/profile';
+import ProgressBar from './progressbar'
+
+export default class App extends Component {
+	
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+	render() {
+		return (
+			<div id="app">
+				{/* <Header /> */}
+				<ProgressBar continuousStart={true}></ProgressBar>
+				<Router onChange={this.handleRoute}>
+					<Home path="/" />
+					<Profile path="/profile/" user="me" />
+					<Profile path="/profile/:user" />
+				</Router>
+			</div>
+		);
+	}
+}
