6e89d4151da800e6a9c5c29b23de53a93c2f0593
eyupkayadarcin <eyup@jointventures.io>
2020-05-16 14:58:08 +0300

    deleted header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,4 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
-
-import Header from './header';
-
-// Code-splitting is automated for routes
-import Home from '../routes/home';
-import Profile from '../routes/profile';
 import ProgressBar from './progressbar'
 
 export default class App extends Component {
@@ -17,13 +10,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				{/* <Header /> */}
 				<ProgressBar continuousStart={true}></ProgressBar>
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
 			</div>
 		);
 	}
